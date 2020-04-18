10e22e30f080e00d5ae17716aa028c572ac90b1e
Bayard75 <bayard.maniraho@gmail.com>
2020-04-18 17:31:15 +0200

    added env variables

--- sub_project/settings.py
+++ sub_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,12 +22,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&1ezp+1$7p@%c2!c1qi0e7+j90cguy28d0%hos#v%_)tc$p=&b'
+SECRET_KEY = os.environ.get('SUB_PROJECT_SK')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG_ENV_VARIABLE')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['purbeurrebayard.herokuapp.com',
+                'localhost']
 
 
 # Application definition
