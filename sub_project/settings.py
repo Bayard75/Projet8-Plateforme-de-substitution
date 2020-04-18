3a3581a5a973877e55269b7bc6a73a1c4f59c615
Bayard75 <bayard.maniraho@gmail.com>
2020-04-18 17:40:27 +0200

    c

--- sub_project/settings.py
+++ sub_project/settings.py
@@ -136,10 +136,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4' 
 
