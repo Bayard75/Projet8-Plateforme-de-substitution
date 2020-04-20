8048b40ae61dbfc3623ba66c2a22cee17cf6d7f8
Bayard75 <bayard.maniraho@gmail.com>
2020-04-20 16:08:11 +0200

    debug variable is now a env variable

--- sub_project/settings.py
+++ sub_project/settings.py
@@ -14,7 +14,6 @@ import os
 import django_heroku
 
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -26,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SUB_PROJECT_SK')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_ENV_VARIABLE')=="'True'")
 
 ALLOWED_HOSTS = ['purbeurrebayard.herokuapp.com',
                 'localhost']
