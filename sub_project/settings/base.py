416c3237332b7c4c2dbcd350feef7545b7c6cf4b
Bayard75 <bayard.maniraho@gmail.com>
2020-05-07 23:34:12 +0200

    created settings folder for diff environement

--- sub_project/settings/base.py
+++ sub_project/settings/base.py
@@ -13,9 +13,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
+from unipath import Path
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).ancestor(3)
 
 
 # Quick-start development settings - unsuitable for production
