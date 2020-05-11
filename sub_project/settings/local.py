ed9c42f84d9053c6c75b1622df0af44922cf9435
Bayard75 <bayard.maniraho@gmail.com>
2020-05-11 19:55:44 +0200

    backsteped

--- sub_project/settings/local.py
+++
@@ -1,7 +0,0 @@
-import os
-from .base import *
-DEBUG = True
-
-SECRET_KEY = os.environ.get('SUB_PROJECT_SK')
-
-ALLOWED_HOSTS = ['*']
--- sub_project/settings/production.py
+++
@@ -1,7 +0,0 @@
-import os
-from .base import *
-DEBUG = False
-
-SECRET_KEY = os.environ.get('SUB_PROJECT_SK')
-
-ALLOWED_HOSTS = ['*']
