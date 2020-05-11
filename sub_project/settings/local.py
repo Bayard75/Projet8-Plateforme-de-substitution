74b172475df1bfdd18bd4e8105e92ca980229a0a
Bayard75 <bayard.maniraho@gmail.com>
2020-05-11 19:50:53 +0200

    added import os

--- sub_project/settings/local.py
+++ sub_project/settings/local.py
@@ -1,5 +1,7 @@
+import os
 from .base import *
-
 DEBUG = True
+
+SECRET_KEY = os.environ.get('SUB_PROJECT_SK')
 
 ALLOWED_HOSTS = ['*']
--- sub_project/settings/production.py
+++ sub_project/settings/production.py
@@ -1,3 +1,7 @@
+import os
 from .base import *
 DEBUG = False
+
+SECRET_KEY = os.environ.get('SUB_PROJECT_SK')
+
 ALLOWED_HOSTS = ['*']
