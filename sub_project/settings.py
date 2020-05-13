05cab0b3d32ccece36d3ffc9648753a3595d3fc6
Bayard75 <bayard.maniraho@gmail.com>
2020-05-13 14:52:53 +0200

    back

--- sub_project/settings.py
+++ sub_project/settings.py
@@ -13,19 +13,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import django_heroku
 
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
-
-
-sentry_sdk.init(
-    dsn="https://99f6bad7a8f8454e837e7c6bb8a79477@o392088.ingest.sentry.io/5239133",
-    integrations=[DjangoIntegration()],
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
